c25c962cbed33ceb7e969d570ffad8322dcb147f
Matt Hopkins <moebaca@hotmail.com>
2022-04-19 09:33:11 -0700

    Init

---
+++ test/sk-site-ecs-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SkSiteEcsCdk from '../lib/sk-site-ecs-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sk-site-ecs-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SkSiteEcsCdk.SkSiteEcsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
